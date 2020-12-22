30963e39dc9d4dd603c5b631c6eaa15e73eb964e
Pavel Fokin <49921254+pfokin92@users.noreply.github.com>
2020-12-22 10:13:04 +0300

    Add files via upload

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,17 @@
+
+let form = document.getElementById("form");
+let progress = document.getElementById("progress");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    var formData = new FormData(form);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    xhr.send(formData);
+
+    xhr.onprogress = function(event) {
+      progress.value = event.loaded / 100000000;
+    }
+})
